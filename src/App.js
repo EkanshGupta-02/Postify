4b26111926024f94862200142e5a1f49dc2442e0
EkanshGupta-02 <2020kucp1081@iiitkota.ac.in>
2023-04-03 10:46:08 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,42 +5,33 @@ import Navbar from "./components/navbar/Navbar";
 import Collection from "./pages/collection/Collection";
 import Home from "./pages/home/Home";
 import ProductDetail from "./pages/productDetail/ProductDetail";
-import {useDispatch} from 'react-redux'
+import { useDispatch } from "react-redux";
 import { fetchCategories } from "./redux/categorySlice";
 import Payments from "./components/payments/Payments";
+import { ToastContainer } from "react-toastify";
 
 function App() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCategories());
+  }, []);
 
-    useEffect(() => {
-        dispatch(fetchCategories())
-    }, [])
-
-    return (
-        <div className="App">
-            <Navbar />
-            <main>
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route
-                        path="/category/:categoryId?"
-                        element={<Collection />}
-                    />
-                    <Route
-                        path="/products/:productId"
-                        element={<ProductDetail />}
-                    />
-                    <Route
-                        path="/payments/:status"
-                        element={<Payments />}
-                    />
-                </Routes>
-            </main>
-
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:categoryId?" element={<Collection />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/payments/:status" element={<Payments />} />
+        </Routes>
+      </main>
+      <Footer />
+      <ToastContainer />
+    </div>
+  );
 }
 
 export default App;
